a22d55e5b03a1e17c91883b031c9d119da722711
Guillermo García <guijujo.25@gmail.com>
2023-11-07 01:52:29 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Index from './componentes/Index';
-import Cotizacion from './componentes/Cotizacion';
-import Presupuestos from './componentes/Presupuestos';
-import CotizadorContext from './contextos/CotizadorContext';
-import PresupuestosContext from './contextos/PresupuestosContext';
-import { useState } from 'react';
-import useLocalstorage from './hooks/useLocalStorage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./componentes/Index";
+import Cotizacion from "./componentes/Cotizacion";
+import Presupuestos from "./componentes/Presupuestos";
+import CotizadorContext from "./contextos/CotizadorContext";
+import PresupuestosContext from "./contextos/PresupuestosContext";
+import { useState } from "react";
+import useLocalstorage from "./hooks/useLocalStorage";
 
 function App() {
-  const [presupuestos, setPresupuestos] = useLocalstorage('presupuestos', []);
+  const [presupuestos, setPresupuestos] = useLocalstorage("presupuestos", []);
   const [elementos, setElementos] = useState({
     metros2: 20,
     propiedad: 0,
@@ -21,15 +21,15 @@ function App() {
         <CotizadorContext.Provider value={{ elementos, setElementos }}>
           <BrowserRouter>
             <Routes>
-              <Route path='/Cotizador' index element={<Index />} />
-              <Route path='/Cotizacion' element={<Cotizacion />} />
-              <Route path='/Presupuestos' element={<Presupuestos />} />
+              <Route path="/Cotizador" index element={<Index />} />
+              <Route path="/Cotizacion" element={<Cotizacion />} />
+              <Route path="/Presupuestos" element={<Presupuestos />} />
             </Routes>
           </BrowserRouter>
         </CotizadorContext.Provider>
       </PresupuestosContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,27 +1,36 @@
-import { Link } from 'react-router-dom';
-import { FaClipboardList } from 'react-icons/fa6';
-import { FaHouse } from 'react-icons/fa6';
-import { FaCalculator } from 'react-icons/fa6';
+import { Link } from "react-router-dom";
+import { FaClipboardList } from "react-icons/fa6";
+import { FaHouse } from "react-icons/fa6";
+import { FaCalculator } from "react-icons/fa6";
 function Navbar() {
-
   return (
     <>
-      <div className='nav'>
-        <input type='checkbox' id='nav-check' />
-        <Link to={'/'} className='nav-header'>
-          <img src="/cotizador/SegurosUNTREF.webp"  alt='Logo empresa' title='Seguros UNTREF' />
+      <div className="nav">
+        <input type="checkbox" id="nav-check" />
+        <Link to={"/"} className="nav-header">
+          <img
+            src="/cotizador/SegurosUNTREF.webp"
+            alt="Logo empresa"
+            title="Seguros UNTREF"
+          />
         </Link>
-        <div className='nav-btn'>
-          <label htmlFor='nav-check'>
+        <div className="nav-btn">
+          <label htmlFor="nav-check">
             <span></span>
             <span></span>
             <span></span>
           </label>
         </div>
-        <div className='nav-links'>
-          <Link to={'/Cotizador'}><FaHouse/> Inicio</Link>
-          <Link to={'/Cotizacion'}><FaCalculator/> Cotizador</Link>
-          <Link to={'/Presupuestos'}><FaClipboardList/> Presupuestos</Link>          
+        <div className="nav-links">
+          <Link to={"/Cotizador"}>
+            <FaHouse /> Inicio
+          </Link>
+          <Link to={"/Cotizacion"}>
+            <FaCalculator /> Cotizador
+          </Link>
+          <Link to={"/Presupuestos"}>
+            <FaClipboardList /> Presupuestos
+          </Link>
         </div>
       </div>
     </>
