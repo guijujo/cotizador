e8dadfb31722e32b0bd78cc78d5de2685f28ac3c
Guillermo García <guijujo.25@gmail.com>
2023-11-07 01:44:48 -0300

    arreglo

--- src/componentes/Index.jsx
+++ src/componentes/Index.jsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 const Index = () => {
-  
+
   return (
     <>
       <Navbar />
       <article className='contenedor'>
       <Link to={'/Cotizacion'}>
-          <img src="/SegurosUNTREF.webp" itemType='image/webp' alt='Logo SeguroUNTREF' title='Logo Seguro UNTREF'/>
+          <img src="/SegurosUNTREF.webp" alt='Logo SeguroUNTREF' title='Logo Seguro UNTREF'/>
         </Link>
         <h1>Seguros de viviendas</h1>
         <p>Cotizamos tu vivienda al mejor precio</p>
