aae232e9368a0c1c07890339047620cbd0e66783
Guillermo García <137415115+guijujo@users.noreply.github.com>
2023-11-07 03:05:12 -0300

    Actualizar Presupuesto.jsx

--- src/componentes/Presupuesto.jsx
+++ src/componentes/Presupuesto.jsx
@@ -5,11 +5,10 @@ import { FaX } from 'react-icons/fa6'
 const Presupuesto = ({ index, fecha, metros2, propiedad, ubicacion, cotizacion, onDelete }) => {
   const [prop, setProp] = useState(null);
   const [ubi, setUbi] = useState(null);
-  const urlAPI = "https://guijujo.github.io/datajson"
 
   useEffect(() => {
     const datos = async () => {
-      let query = await (await fetch(urlAPI)).json();
+      let query = await (await fetch(/cotizador/data.json)).json();
       setProp(query.find(({ factor }) => factor === propiedad));
       setUbi(query.find(({ factor }) => factor === ubicacion));
     };
