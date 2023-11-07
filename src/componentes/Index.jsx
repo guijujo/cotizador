f33cba56195e02d18081db8fbc2e9f86d365d6b4
Guillermo García <guijujo.25@gmail.com>
2023-11-07 01:07:06 -0300

    imagen

--- src/componentes/Index.jsx
+++ src/componentes/Index.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 const Index = () => {
-  const logo = '/public/SegurosUNTREF.webp';
+  const logo = './public/SegurosUNTREF.webp';
   return (
     <>
       <Navbar />
