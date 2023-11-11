e7d8f11ef741d4e270bf2d4f84495c54ed01d56d
Guillermo García <guijujo.25@gmail.com>
2023-11-11 14:42:58 -0300

    cambios app rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-import Home from "./componentes/index";
-import Cotizacion from "./componentes/cotizacion";
-import Presupuestos from "./componentes/presupuestos";
-import CotizadorContext from "./contextos/cotizadorContext";
-import PresupuestosContext from "./contextos/presupuestosContext";
-import useLocalstorage from "./hooks/useLocalStorage";
+import Home from "/src/componentes/index";
+import Cotizacion from "/src/componentes/cotizacion";
+import Presupuestos from "/src/componentes/presupuestos";
+import CotizadorContext from "/src/contextos/cotizadorContext";
+import PresupuestosContext from "/src/contextos/presupuestosContext";
+import useLocalstorage from "/src/hooks/useLocalStorage";
 import { useState } from "react";
 
 function App() {
